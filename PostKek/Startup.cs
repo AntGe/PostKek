dc686ff7da2c438071d474d32f1f5d339ac3124d
doomat <1127309371>
2017-05-05 12:42:22 +0300

    Init commit of project

---
+++ PostKek/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PostKek.Startup))]
+namespace PostKek
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
